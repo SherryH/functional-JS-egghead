f476e92a62f103f7d90401103676887032049fed
SherryH <enjoy.wind7rain@gmail.com>
2018-06-15 11:10:30 +1200

    Completed the either examples

--- examples/05-either-examples.js
+++ examples/05-either-examples.js
@@ -1,196 +1,227 @@
-const fs = require('fs')
+const fs = require("fs");
 
 // helper functions
-const Right = x => (
-  {
-    chain: f => f(x),
-    map: f => Right(f(x)),
-    fold: (f, g) => g(x),
-    inspect: () => `Right(${x})`
-  }
-)
-const Left = x => (
-  {
-    chain: f => Left(x),
-    map: f => Left(x),
-    fold: (f, g) => f(x),
-    inspect: () => `Left(${x})`
-  }
-)
-const fromNullable = x =>
-  x != null ? Right(x) : Left(null)
-const tryCatch = f => {
-  try {
-    return Right(f())
-  } catch (e) {
-    return Left(e)
-  }
-}
-
+const Right = x => ({
+  chain: f => f(x),
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`
+});
+const Left = x => ({
+  chain: f => Left(x),
+  map: f => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`
+});
+const fromNullable = x => (x != null ? Right(x) : Left(null));
+// const tryCatch = f => {
+//   try {
+//     return Right(f());
+//   } catch (e) {
+//     return Left(e);
+//   }
+// };
 
 // mocked functions
-const renderPage = user => `renderPage(${user})`
-const showLogin = () => `showLogin()`
+const renderPage = user => `renderPage(${user})`;
+const showLogin = () => `showLogin()`;
 
 // imperative coding style
 const openSite = () => {
-  if(currentUser) {
-    return renderPage(currentUser)
+  if (currentUser) {
+    return renderPage(currentUser);
   } else {
-    return showLogin()
+    return showLogin();
   }
-}
+};
 
-// functional coding style
-const openSiteFn = currentUser =>
+const eitherIsNull = currentUser => {
+  return currentUser == null ? Left(currentUser) : Right(currentUser);
+};
 
-  // Right if currentUser is defined, Left otherwise
-  fromNullable(currentUser)
+const openSiteFn = currentUser => {
+  return eitherIsNull(currentUser).fold(showLogin, () =>
+    renderPage(currentUser)
+  );
+};
 
-    // Left values are passed to the left function, Right values are passed to the right
-    .fold(showLogin, renderPage)
+// // functional coding style
+// const openSiteFn = currentUser =>
+
+//   // Right if currentUser is defined, Left otherwise
+//   fromNullable(currentUser)
+
+//     // Left values are passed to the left function, Right values are passed to the right
+//     .fold(showLogin, renderPage)
 
 // testing
-console.log(`openSiteFn() : `, openSiteFn())
-console.log(`openSiteFn('user') : `, openSiteFn('user'))
-
+console.log(`openSiteFn() : `, openSiteFn());
+console.log(`openSiteFn('user') : `, openSiteFn("user"));
 
 // mocked functions
-const defaultPrefs = () => `defaultPrefs()`
-const loadPrefs = prefs => `loadPrefs(${prefs})`
+const defaultPrefs = x => `defaultPrefs(${x})`;
+const loadPrefs = prefs => `loadPrefs(${prefs})`;
 
 // imperative
 const getPrefs = user => {
-  if(user.premium) {
-    return loadPrefs(user.preferences)
+  if (user.premium) {
+    return loadPrefs(user.preferences);
   } else {
-    return defaultPrefs()
+    return defaultPrefs();
   }
-}
+};
 
-// functional
 const getPrefsFn = user =>
-  (user.premium ? Right(user) : Left('not premium'))
+  (user.premium ? Right(user) : Left("not premium")).fold(defaultPrefs, user =>
+    loadPrefs(user.preferences)
+  );
+// // functional
+// const getPrefsFn = user =>
+//   (user.premium ? Right(user) : Left('not premium'))
 
-    // acts on Right values and ignores Left values
-    .map(u => u.preferences)
-    .fold(defaultPrefs, loadPrefs)
+//     // acts on Right values and ignores Left values
+//     .map(u => u.preferences)
+//     .fold(defaultPrefs, loadPrefs)
 
 // testing
-console.log(`getPrefsFn({}) : `, getPrefsFn({}))
-console.log(`getPrefsFn({premium: true, preferences: 'some_prefs'}) : `, getPrefsFn({premium: true, preferences: 'some_prefs'}))
-
-
+console.log(`getPrefsFn({}) : `, getPrefsFn({}));
+console.log(
+  `getPrefsFn({premium: true, preferences: 'some_prefs'}) : `,
+  getPrefsFn({ premium: true, preferences: "some_prefs" })
+);
 
 // imperative
 const streetName = user => {
-  const address = user.address
+  const address = user.address;
 
-  if(address) {
-    const street = address.street
+  if (address) {
+    const street = address.street;
 
-    if(street) {
-      return street.name
+    if (street) {
+      return street.name;
     }
   }
-  return 'no street'
-}
+  return "no street";
+};
 
-// functional
 const streetNameFn = user =>
-  fromNullable(user.address)
+  eitherIsNull(user.address)
+    .chain(addrs => eitherIsNull(addrs.street))
+    .map(street => street.name)
+    .fold(() => "no street", name => name);
 
-    // chain expects function from raw value into the wrapped one
-    // only applies to Right values
-    .chain(ad => fromNullable(ad.street))
-    .map(st => st.name)
-    .fold(er => 'no street', name => name)
+// // functional
+// const streetNameFn = user =>
+//   fromNullable(user.address)
+//     // chain expects function from raw value into the wrapped one
+//     // only applies to Right values
+//     .chain(ad => fromNullable(ad.street))
+//     .map(st => st.name)
+//     .fold(er => "no street", name => name);
 
 // testing
-console.log(`streetNameFn({}) : `, streetNameFn({}))
-console.log(`streetNameFn({address: '1'}) : `, streetNameFn({address: '1'}))
+console.log(`streetNameFn({}) : `, streetNameFn({}));
+console.log(`streetNameFn({address: '1'}) : `, streetNameFn({ address: "1" }));
 console.log(
-  `streetNameFn({address: {street: {name: 'some_street'}}) : `, 
-  streetNameFn({address: {street: {name: 'some_street'}}})
-)
-
-
+  `streetNameFn({address: {street: {name: 'some_street'}}) : `,
+  streetNameFn({ address: { street: { name: "some_street" } } })
+);
 
 // imperative
 const concatUniq = (x, ys) => {
-  const found = ys.filter(y => y === x)[0]
-  return found ? ys : ys.concat(x)
-}
+  const found = ys.filter(y => y === x)[0];
+  return found ? ys : ys.concat(x);
+};
 
 // functional
 const concatUniqFn = (x, ys) =>
-  fromNullable(ys.filter(y => y === x)[0])
-    .fold(() => ys.concat(x), () => ys)
+  eitherIsNull(ys.filter(y => y === x)[0]).fold(() => ys.concat(x), () => ys);
+// const concatUniqFn = (x, ys) =>
+//   fromNullable(ys.filter(y => y === x)[0]).fold(() => ys.concat(x), () => ys);
 
 // testing
-console.log(`concatUniqFn(1, [2,3]) : `, concatUniqFn(1, [2,3]))
-console.log(`concatUniqFn(2, [2,3]) : `, concatUniqFn(2, [2,3]))
-
-
+console.log(`concatUniqFn(1, [2,3]) : `, concatUniqFn(1, [2, 3]));
+console.log(`concatUniqFn(2, [2,3]) : `, concatUniqFn(2, [2, 3]));
 
 // mocked
 
-
 // imperative
 const wrapExample = example => {
-  if(example.previewPath) {
+  if (example.previewPath) {
     try {
-      example.preview = fs.readFileSync(example.previewPath)
-    } catch(e) { }
+      example.preview = fs.readFileSync(example.previewPath);
+    } catch (e) {}
   }
-  return example
-}
+  return example;
+};
+
+const tryCatch = func => {
+  try {
+    return Right(func());
+  } catch (e) {
+    return Left(e);
+  }
+};
 
 // functional
-const readFileFn = path => tryCatch(() => fs.readFileSync(path))
+const readFileFn = path => tryCatch(() => fs.readFileSync(path));
 
 // functional
 const wrapExampleFn = example =>
-  fromNullable(example.previewPath)
-    .chain(readFileFn)
-    .fold(
-      () => example,
-      preview => Object.assign({}, example, {preview})
-    )
+  eitherIsNull(example.previewPath)
+    .chain(path => tryCatch(() => fs.readFileSync(path)))
+    .chain(c => tryCatch(() => c.toString()))
+    .fold(() => example, preview => ({ ...example, preview }));
+// const wrapExampleFn = example =>
+//   fromNullable(example.previewPath)
+//     .chain(readFileFn)
+//     .fold(() => example, preview => Object.assign({}, example, { preview }));
 
 // testing
-console.log(`wrapExampleFn({previewPath: 'no_file.txt'}) : `, wrapExampleFn({previewPath: 'no_file.txt'}))
-console.log(`wrapExampleFn({previewPath: 'preview.txt'}) : `, wrapExampleFn({previewPath: 'preview.txt'}))
-
-
+console.log(
+  `wrapExampleFn({previewPath: 'no_file.txt'}) : `,
+  wrapExampleFn({ previewPath: "no_file.txt" })
+);
+console.log(
+  `wrapExampleFn({previewPath: 'preview.txt'}) : `,
+  wrapExampleFn({ previewPath: "preview.txt" })
+);
 
 // imperative
 const parseDbUrl = cfg => {
   try {
-    const c = JSON.parse(cfg)
-    if(c.url) {
-      return c.url.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+    const c = JSON.parse(cfg);
+    if (c.url) {
+      return c.url.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
     }
   } catch (e) {
-      return null
+    return null;
   }
-}
+};
 
 // functional
 const parseDbUrlFn = cfg =>
   tryCatch(() => JSON.parse(cfg))
-    .chain(c => fromNullable(c.url))
+    .chain(c => eitherIsNull(c.url))
     .fold(
-      e => `config is invalid`,
-      u => u.match(/postgresql:\/\/([^:]+):([^@]+)@([^:]+)/)
-    )
+      () => null,
+      url => url.match(/postgresql:\/\/([^:]+):([^@]+)@([^:]+)/)
+    );
+
+// const parseDbUrlFn = cfg =>
+//   tryCatch(() => JSON.parse(cfg))
+//     .chain(c => fromNullable(c.url))
+//     .fold(
+//       e => `config is invalid`,
+//       u => u.match(/postgresql:\/\/([^:]+):([^@]+)@([^:]+)/)
+//     );
 
 // testing
 console.log(
   `parseDbUrlFn('{"no_url": "invalid"}') : `,
   parseDbUrlFn('{"no_url": "invalid"}')
-)
+);
 console.log(
   `parseDbUrlFn('{"url": "postgresql://user:secret@localhost"}') : `,
   parseDbUrlFn('{"url": "postgresql://user:secret@localhost"}')
-)
+);
