01c2c8145286cc32b3815dee102b2adbee950905
SherryH <enjoy.wind7rain@gmail.com>
2018-06-18 08:01:28 +1200

    Completed the semigroup

--- examples/06-semigroups-types.js
+++ examples/06-semigroups-types.js
@@ -1,69 +1,71 @@
 // https://github.com/DrBoolean/immutable-ext
 // Map delegates 'concat' method to its values:
 // https://github.com/DrBoolean/immutable-ext/blob/master/browser_dist/index.js#L51
-const { Map, List } = require('immutable-ext')
+const { Map, List } = require("immutable-ext");
 
 // helper functions
-const Right = x => (
-  {
-    chain: f => f(x),
-    map: f => Right(f(x)),
-    fold: (f, g) => g(x),
-    inspect: () => `Right(${x})`
-  }
-)
-const Left = x => (
-  {
-    chain: f => Left(x),
-    map: f => Left(x),
-    fold: (f, g) => f(x),
-    inspect: () => `Left(${x})`
-  }
-)
+const Right = x => ({
+  chain: f => f(x),
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`
+});
+const Left = x => ({
+  chain: f => Left(x),
+  map: f => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`
+});
 
+// examples: Array: [1,2].concat([3]) => [1,2,3]
+// String: 'a'.concat('b') => 'ab'
+
+// Sum: Sum(1).concat(Sum(3)) => Sum(4)
 
 // Sum container adds semigroup structure
 // via the 'concat' method
 const Sum = x => ({
+  x: x,
+  concat: ({ x: val }) => Sum(x + val),
+  inspect: () => `Sum(${x})`
+});
+// const Sum = x => ({
 
-  //  make x accessible via the 'x' prop
-  x,
+//   //  make x accessible via the 'x' prop
+//   x,
 
-  // destructuring value for the key 'x'
-  // equivalent to y = argument.x
-  // Usage: Sum(n).concat(Sum(m)) //=> Sum(n+m)
-  concat: ({x: val}) => Sum(x + val),
+//   // destructuring value for the key 'x'
+//   // equivalent to y = argument.x
+//   // Usage: Sum(n).concat(Sum(m)) //=> Sum(n+m)
+//   concat: ({x: val}) => Sum(x + val),
 
-  // custom getter used by console.log
-  inspect: _ => `Sum(${x})`
-})
+//   // custom getter used by console.log
+//   inspect: _ => `Sum(${x})`
+// })
 
-console.log(
-  'Sum(22).concat(Sum(33)): ',
-  Sum(22).concat(Sum(33))
-)
-
+console.log("Sum(22).concat(Sum(33)): ", Sum(22).concat(Sum(33)));
 
 // All container add logical (boolean) 'concat'
+// true && false => false
+// true && true => true
+
 const All = x => ({
-  x,
-  concat: ({x: val}) => All(x && val),
+  x: x,
+  concat: ({ x: val }) => All(x && val),
+  inspect: () => `All(${x})`
+});
 
-  // custom getter used by console.log
-  inspect: _ => `All(${x})`
-})
+// const All = x => ({
+//   x,
+//   concat: ({ x: val }) => All(x && val),
 
-console.log(
-  'All(true).concat(All(false)): ',
-  All(true).concat(All(false))
-)
+//   // custom getter used by console.log
+//   inspect: _ => `All(${x})`
+// });
 
-console.log(
-  'All(true).concat(All(true)): ',
-  All(true).concat(All(true))
-)
+console.log("All(true).concat(All(false)): ", All(true).concat(All(false)));
 
-
+console.log("All(true).concat(All(true)): ", All(true).concat(All(true)));
 
 const First = x => ({
   x,
@@ -73,54 +75,50 @@ const First = x => ({
 
   // custom getter used by console.log
   inspect: _ => `First(${x})`
-})
-
+});
 
 console.log(
   `First('I am the first').concat(First('I am the second')) : `,
-  First('I am the first').concat(First('I am the second'))
-)
-
+  First("I am the first").concat(First("I am the second"))
+);
 
 // use Map to combine the 'concats' for different types
 const acct1 = Map({
-  name: First('Nice guy'),
+  name: First("Nice guy"),
   isPaid: All(true),
   points: Sum(10),
-  friends: ['Franklin']
-})
+  friends: ["Franklin"]
+});
 
 const acct2 = Map({
-  name: First('Another guy'),
+  name: First("Another guy"),
   isPaid: All(false),
   points: Sum(2),
-  friends: ['Gatsby']
-})
-
+  friends: ["Gatsby"]
+});
 
 // The semigroup structure combines both objects componentwise
 // individual custom 'concat' applies in each component
-const res = acct1.concat(acct2)
+const res = acct1.concat(acct2);
 
 // testing
-console.log(
-  `acct1.concat(acct2) : `,
-  res.toJS()
-)
-
-
+console.log(`acct1.concat(acct2) : `, res.toJS());
 
 // List from 'immutable-ext' implements 'foldMap(f, empty)'
 // which runs 'reduce' over the 'concat' of the target type of 'f'
 console.log(
   `List([1,2,3]).foldMap(x => [x, 10]) : `,
-  List([1,2,3]).foldMap(x => [x, 10])
-)
+  List([1, 2, 3]).foldMap(x => [x, 10])
+);
 console.log(
   `List([]).foldMap(x => [x, 10], []) : `,
   List([]).foldMap(x => [x, 10], [])
-)
+);
 console.log(
   `List([1,2,3]).foldMap(x => First(x)) : `,
-  List([1,2,3]).foldMap(x => First(x))
-)
+  List([1, 2, 3]).foldMap(x => First(x))
+);
+console.log(
+  `List([1,2,3]).foldMap(x => Sum(x)) : `,
+  List([1, 2, 3]).foldMap(x => Sum(x))
+);
